9da2da8098b9c3a41b9c06e6c648dfe0ae007578
kaitlynmccalebb <45012138+kaitlynmccalebb@users.noreply.github.com>
2020-01-17 13:58:13 -0800

    Add files via upload

---
+++ ashamedomarapollo.rb
@@ -0,0 +1,42 @@
+# Welcome to Sonic Pi v3.1
+play chord(:E, :minor7)
+sleep 0.25
+play chord(:B, :minor7)
+sleep 0.25
+play chord(:A, :major7)
+sleep 0.25
+play chord(:A, :minor7)
+sleep 0.25
+play chord(:A, "7")
+sleep 0.25
+play chord(:C, :major7)
+sleep 0.25
+play chord(:B, :major7)
+sleep 0.25
+play chord(:G, :major7)
+sleep 0.25
+play chord(:Ab, :minor7)
+sleep 0.25
+play chord(:C, "7")
+sleep 0.25
+play chord(:G, "7")
+sleep 0.25
+play chord(:D, "7")
+sleep 0.25
+play chord(:D, :minor7)
+sleep 0.25
+play chord(:F, :minor7)
+sleep 0.25
+play chord(:F, :major7)
+sleep 0.25
+play chord(:Cs, :major7)
+sleep 0.25
+play chord(:As, :minor7)
+sleep 0.25
+play chord(:As, :major7)
+sleep 0.25
+play chord(:E, :major7)
+sleep 0.25
+play chord(:Ds, :major7)
+sleep 0.25
+play chord(:Gs, :major7)
