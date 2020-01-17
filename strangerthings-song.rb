b27f44066ab59f5686dd0074f284211578829941
kaitlynmccalebb <45012138+kaitlynmccalebb@users.noreply.github.com>
2020-01-17 13:46:43 -0800

    Add files via upload

---
+++ strangerthings-song.rb
@@ -0,0 +1,20 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 160
+use_synth :chipbass
+live_loop :stranger do
+  play :C4
+  sleep 0.5
+  play :E4
+  sleep 0.5
+  play :G4
+  sleep 0.5
+  play :B4
+  sleep 0.5
+  play :C5
+  sleep 0.5
+  play :B4
+  sleep 0.5
+  play :G4
+  sleep 0.5
+  play :E4
+end
