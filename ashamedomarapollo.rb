ca88279f5e2b2b2c231da56f6ed1bc3c8b36726e
kaitlynmccalebb <45012138+kaitlynmccalebb@users.noreply.github.com>
2020-01-22 09:12:18 -0800

    Add files via upload

--- ashamedomarapollo.rb
+++ ashamedomarapollo.rb
@@ -1,42 +1,70 @@
 # Welcome to Sonic Pi v3.1
-play chord(:E, :minor7)
-sleep 0.25
-play chord(:B, :minor7)
-sleep 0.25
-play chord(:A, :major7)
-sleep 0.25
-play chord(:A, :minor7)
-sleep 0.25
-play chord(:A, "7")
-sleep 0.25
-play chord(:C, :major7)
-sleep 0.25
-play chord(:B, :major7)
-sleep 0.25
-play chord(:G, :major7)
-sleep 0.25
-play chord(:Ab, :minor7)
-sleep 0.25
-play chord(:C, "7")
-sleep 0.25
-play chord(:G, "7")
-sleep 0.25
-play chord(:D, "7")
-sleep 0.25
-play chord(:D, :minor7)
-sleep 0.25
-play chord(:F, :minor7)
-sleep 0.25
-play chord(:F, :major7)
-sleep 0.25
-play chord(:Cs, :major7)
-sleep 0.25
-play chord(:As, :minor7)
-sleep 0.25
-play chord(:As, :major7)
-sleep 0.25
-play chord(:E, :major7)
-sleep 0.25
-play chord(:Ds, :major7)
-sleep 0.25
-play chord(:Gs, :major7)
+use_bpm 91
+live_loop :bass do
+  use_synth :fm
+  4.times do
+    play chord(:E, :minor)
+    sleep 0.25
+  end
+  sample"C:/Users/kaitlyn_mccalebb/Downloads/dontcare_sample.wav"
+  sleep 8
+  play chord(:B, :minor)
+  sleep 1
+  play chord(:E, :minor)
+  sleep 1
+  play chord(:B, :minor)
+  sleep 1
+  play chord(:A, "7")
+  sleep 1
+  play chord(:B, :minor)
+  sleep 0.25
+  play chord(:A, :minor)
+  sleep 0.5
+  play chord(:E, :minor)
+  sleep 0.5
+  play chord(:B, :minor7)
+  sleep 0.5
+  play chord(:A, :minor)
+  sleep 0.5
+  play chord(:E, :minor)
+  sleep 0.5
+  play chord(:C, "7")
+  sleep 0.5
+  play chord(:B, "7")
+  sleep 0.5
+  play chord(:A, :minor)
+  sleep 0.5
+  play chord(:E, :minor)
+  sleep 0.5
+  play chord(:B, :minor)
+  sleep 0.5
+  play chord(:A, :minor)
+  sleep 0.5
+  play chord(:E, :minor)
+  sleep 0.5
+  play chord(:B, :minor)
+  sleep 0.5
+  play chord(:A, :minor)
+  sleep 0.5
+  play chord(:E, :minor)
+  sleep 0.5
+  play chord(:B, :minor)
+  sleep 0.5
+  play chord(:A, :minor)
+  sleep 0.5
+  play chord(:E, :minor)
+  sleep 0.5
+  play chord(:B, :minor)
+  sleep 0.5
+  play chord(:A, :minor)
+  sleep 0.5
+  play chord(:E, :minor)
+  sleep 0.5
+  play chord(:B, :minor)
+  sleep 0.5
+  play chord(:A, :minor)
+  sleep 0.5
+  play chord(:G, "7")
+  sleep 0.5
+  play chord(:Gs, :major7)
+end
