0c6d1ff0d0d3aaf2c70cbcb7b503581d0bc10962
kaitlynmccalebb <45012138+kaitlynmccalebb@users.noreply.github.com>
2020-01-31 13:59:11 -0800

    Add files via upload

--- newcustom.rb
+++ newcustom.rb
@@ -1,11 +1,21 @@
 # Welcome to Sonic Pi
 jazz = "C:/Users/kaitlyn_mccalebb/Downloads/jazzhiphop.wav"
 beat ="C:/Users/kaitlyn_mccalebb/Downloads/gunnabeat.wav"
+trap ="C:/Users/kaitlyn_mccalebb/Downloads/gangtrapbeat.wav"
 intro="C:/Users/kaitlyn_mccalebb/Downloads/beginningsample1.wav"
-glitch= "C:/Users/kaitlyn_mccalebb/Downloads/glitchsoundeffect.wav"
+glitch= "C:/Users/kaitlyn_mccalebb/Downloads/newglitch.wav"
 kanye= "C:/Users/kaitlyn_mccalebb/Downloads/kanye.wav"
+woah= "C:/Users/kaitlyn_mccalebb/Downloads/woah.wav"
+pause= "C:/Users/kaitlyn_mccalebb/Downloads/pause2.wav"
+rap="C:/Users/kaitlyn_mccalebb/Downloads/rap.wav"
+rap2= "C:/Users/kaitlyn_mccalebb/Downloads/rap2.wav"
+chill= "C:/Users/kaitlyn_mccalebb/Downloads/chill.wav"
+soft="C:/Users/kaitlyn_mccalebb/Downloads/softvocal.wav"
+nas="C:/Users/kaitlyn_mccalebb/Downloads/ruledtheworld.wav"
+lh="C:/Users/kaitlyn_mccalebb/Downloads/ifiruledtheworld.wav"
+slide="C:/Users/kaitlyn_mccalebb/Downloads/slide.wav"
 
-
+x= 1
 sample intro
 sleep 3
 4.times do
@@ -21,27 +31,45 @@ end
   sleep 0.5
 end
 6.times do
-  play :G, amp: 0.5
-  sample :bd_boom
+  play :G, amp: 0.25
+  sample :bd_boom, amp:x
   sleep 0.25
+  x=x-0.15
 end
-sleep 1
-live_loop :beat do
-  2.times do
-    sample beat, amp: 2
-    sleep 30
-  end
-  stop
-end
-sleep 10
-sample kanye, rate: 1, amp: 0.5
-sleep 51
+x=1
+sample beat, amp: 2
+sleep 29.772
 sample glitch
+sleep 0.2
+sample pause
+sleep 0.2
+sample nas
+sleep 7.930634920634921
 live_loop :change do
-  sample jazz
+  sample chill, amp: 2
+  sleep 53.843650793650795
+end
+sleep 3
+sample soft, amp: 5
+sleep 9.25045351473923
+sample slide
+live_loop :backvocals do
+  sleep 6
+  sample lh, amp: 0.75
+end
+sleep 60
+2.times do
+  sample lh
+end
+live_loop :her do
+  4.times do
+    sample lh, rate:x
+    x=x+0.5
+  end
 end
 
 
+print sample_duration jazz
 
 
 
